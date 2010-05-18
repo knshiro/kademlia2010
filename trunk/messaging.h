7b0e077681cbc4d9823ae47107e59df4412f1fbc
olivier.marcellin7 <olivier.marcellin7@4048e85e-725b-b780-968e-0ebbc6faa551>
2010-05-18 07:45:18 +0000

    first version of print routing table. to be continued....

--- trunk/messaging.h
+++ trunk/messaging.h
@@ -3,6 +3,31 @@
 #define RTLP_MAX_PAYLOAD_SIZE	1024
 #define RTLP_MAX_SEND_BUF_SIZE	1024
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include "json-c-0.9/json.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int _kdm_debug;
+
+struct kademMessage {
+    json_object *header;
+    int payloadLength;    // Length of the payload and header
+    char payload[4000];    // Payload of packet
+};
+
+
+struct dhtMachine {
+   int port;
+   char* address_ip;
+
+};
+
+
 //Packet to send
 struct pkbuf {
 	//struct rtlp_hdr hdr;		/* Header of the packet */
@@ -24,3 +49,20 @@ struct rtlp_client_pcb {
 
 };
 
+
+int kademSendMessage(int sockfd, struct kademMessage *message, char * dst_addr, int dst_port);
+struct kademMessage kademUdpToMessage(char * udpPacket, int length);
+void kdm_debug(const char *msg, ...);
+int print_routing_table(struct rtlp_client_pcb* cpcb, struct dhtMachine* dhtmachine);
+
+
+
+/* Message types */
+extern const char * const KADEM_QUERY;   
+extern const char * const KADEM_ANSWER; 
+
+/*Message query types */
+extern const char * const KADEM_PING;
+extern const char * const KADEM_STORE;
+extern const char * const KADEM_FIND_NODE;
+extern const char * const KADEM_FIND_VALUE;
